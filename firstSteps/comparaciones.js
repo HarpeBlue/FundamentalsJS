d2434e136a8e0ff2420c902fd49c58e4ed8e8dc5
HarpeBlue <hspe2507@gmail.com>
2020-12-15 21:43:29 -0500

    add: comparaciones

---
+++ firstSteps/comparaciones.js
@@ -0,0 +1,23 @@
+var x = 4, y = '4';
+
+x == y; // es true por que los lleva a un mismo tipo de dato.
+
+x === y // false por que verifica el tipo de dato y el valos perse, bien recomendado.
+
+var harold = {
+  nombre: 'Harold'
+}
+
+var otraPersona = {
+  nombre: 'Harold'
+}
+
+// si se comparan objetos pregunta por la referencia donde está guardada entonces ahí si da true
+
+var otraPersona = harold; // apunta al mismo lugar de memoria dando true cuando se compara.
+
+// así desgloces un objeto estás creando uno nuevo por lo tanto está apuntando a un nuevo lugar.
+
+var otraPersona = {
+  ...harold
+}
