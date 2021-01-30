12d91f99661e378bd35a801f26ab5a5017142387
HarpeBlue <hspe2507@gmail.com>
2021-01-30 18:18:53 -0500

    add: Switch

---
+++ 2-estructurasControl/7-switch.js
@@ -0,0 +1,17 @@
+let signo = prompt('¿Cuál es tu signo?');
+
+switch (signo.toLowerCase()) {
+  case 'acuario':
+    console.log('Intente alejarse de las mujeres para virvir tranquilo');
+    break;
+  case 'libra':
+    console.log('Intente alejarse de las mujeres para virvir tranquilo');
+    break;
+  case 'geminis':
+  case 'géminis':
+    console.log('Intente alejarse de las mujeres para virvir tranquilo');
+    break;
+  default:
+    console.log('La entrada es incorrecta');
+    break;
+}
