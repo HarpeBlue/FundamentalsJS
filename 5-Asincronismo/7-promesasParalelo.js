373a3a4a1bedaaebbb3dbfa70bbd1e3bb5a927f1
HarpeBlue <hspe2507@gmail.com>
2021-02-01 22:59:31 -0500

    add: promesas con async await

--- 5-Asincronismo/7-promesasParalelo.js
+++ 5-Asincronismo/7-promesasParalelo.js
@@ -17,27 +17,16 @@ function onError(id) {
   console.log(`Sucedió un error al obtener el personaje ${id}`)
 }
 
-const ids = [1, 2, 3, 4, 5, 6, 7];
+async function obtenerPersonajes() {
+  const ids = [1, 2, 3, 4, 5, 6, 7];
+  const promesas = ids.map((id) => obtenerPersonaje(id))
+  try {
+    const personajes = await Promise.all(promesas);
+    console.log(personajes);
+  } catch (id) {
+    onError(id)
+  }
+}
 
-const promesas = ids.map((id) => obtenerPersonaje(id))
+obtenerPersonajes();
 
-Promise
-  .all(promesas)
-  .then((personajes) => {
-    console.log(personajes)
-  })
-  .catch(onError);
-
-// Las promesas podemos encadenarlas en paralelo por encima de los callbacks
-
-// obtenerPersonaje(1)
-//   .then((character) => {
-//     console.log(`El personaje 1 es ${character.result.properties.name}`)
-//     return obtenerPersonaje(2);
-//   })
-//   .then((character) => {
-//     console.log(`El personaje 2 es ${character.result.properties.name}`)
-//   })
-//   .catch(onError)
-
-// para retornar otra promesa usamos el key word return.
